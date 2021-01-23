78ab9a5930a3e536467538b103d9ae3212aec0bf
Patrick Ofili <patrickofili16@gmail.com>
2021-01-23 22:27:37 +0100

    added search key

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -2,24 +2,23 @@ import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Loader from "./Loader";
 import ApiTable from "./ApiTable";
-import Highlighter from 'react-highlight-words';
+import Highlighter from "react-highlight-words";
 import { SearchOutlined } from "@ant-design/icons";
 import { Input, Button, Space } from "antd";
 
-
 const Wrapper = () => {
   const [loading, setLoading] = useState(true);
-    const [apiData, setApiData] = useState([]);
-    const [searchText, setSearchText] = useState("");
-    const [searchedColumn, setSearchedColumn] = useState("");
-    let searchInput = "";
-    
+  const [apiData, setApiData] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  let searchInput = "";
+
   const addKey = (arr) => {
     for (let i = 0; i < 1000; i++) {
       if (i >= arr.length) {
         break;
       }
-      let pair = { key: i + 1 };
+      let pair = { key: (i + 1).toString() };
       arr[i] = { ...pair, ...arr[i] };
     }
   };
@@ -37,87 +36,83 @@ const Wrapper = () => {
     fetchData();
   }, []);
 
-     const handleSearch = (selectedKeys, confirm, dataIndex) => {
-         confirm();
-         setSearchText(selectedKeys[0])
-        setSearchedColumn(dataIndex)
-     };
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
 
-     const handleReset = (clearFilters) => {
-         clearFilters();
-         setSearchText("");
-     };
-     const getColumnSearchProps = (dataIndex) => ({
-       filterDropdown: ({
-         setSelectedKeys,
-         selectedKeys,
-         confirm,
-         clearFilters,
-       }) => (
-         <div style={{ padding: 8 }}>
-           <Input
-             ref={(node) => {
-               searchInput = node;
-             }}
-             placeholder={`Search ${dataIndex}`}
-             value={selectedKeys[0]}
-             onChange={(e) =>
-               setSelectedKeys(e.target.value ? [e.target.value] : [])
-             }
-             onPressEnter={() =>
-               handleSearch(selectedKeys, confirm, dataIndex)
-             }
-             style={{ width: 188, marginBottom: 8, display: "block" }}
-           />
-           <Space>
-             <Button
-               type="primary"
-               onClick={() =>
-                 handleSearch(selectedKeys, confirm, dataIndex)
-               }
-               icon={<SearchOutlined />}
-               size="small"
-               style={{ width: 90 }}
-             >
-               Search
-             </Button>
-             <Button
-               onClick={() => handleReset(clearFilters)}
-               size="small"
-               style={{ width: 90 }}
-             >
-               Reset
-             </Button>
-           </Space>
-         </div>
-       ),
-       filterIcon: (filtered) => (
-         <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
-       ),
-       onFilter: (value, record) =>
-         record[dataIndex]
-           ? record[dataIndex]
-               .toString()
-               .toLowerCase()
-               .includes(value.toLowerCase())
-           : "",
-       onFilterDropdownVisibleChange: (visible) => {
-         if (visible) {
-           setTimeout(() => searchInput.select(), 100);
-         }
-       },
-       render: (text) =>
-           searchedColumn === dataIndex ? (
-           <Highlighter
-             highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
-             searchWords={[searchText]}
-             autoEscape
-             textToHighlight={text ? text.toString() : ""}
-           />
-         ) : (
-           text
-         ),
-     });
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={(node) => {
+            searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase())
+        : "",
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
 
   const columns = [
     {
@@ -126,6 +121,7 @@ const Wrapper = () => {
       key: "key",
       fixed: "left",
       width: 60,
+      ...getColumnSearchProps("key"),
     },
     {
       title: "FirstName",
